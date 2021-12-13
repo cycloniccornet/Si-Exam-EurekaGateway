9f7c234411e0dc9c2f192650df13b3b6a5cb776b
cycloniccornet <55379577+cycloniccornet@users.noreply.github.com>
2021-12-13 13:18:33 +0100

    Initial commit

---
+++ src/main/java/siEurekagateway/SiExamEurekaGatewayApplication.java
@@ -0,0 +1,16 @@
+package siEurekagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SiExamEurekaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiExamEurekaGatewayApplication.class, args);
+    }
+
+}
